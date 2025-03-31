453ef186c359809f1df449820dbfefde9c4d310a
Sharad Gandhi <sharad.gandhi@gmail.com>
2025-03-30 18:47:40 -0700

    Move vault path to config file and remove hardcoded paths

---
+++ config.template.js
@@ -0,0 +1,11 @@
+// Copy this file to config.js and update the path to your Obsidian vault
+module.exports = {
+    // Path to your Obsidian vault's plugins directory
+    // For macOS with iCloud sync:
+    // vaultPath: '/Users/username/Library/Mobile Documents/iCloud~md~obsidian/Documents/Your Vault/.obsidian/plugins'
+    // For Windows:
+    // vaultPath: 'C:\\Users\\username\\Documents\\Obsidian Vault\\.obsidian\\plugins'
+    // For Linux:
+    // vaultPath: '/home/username/Documents/Obsidian Vault/.obsidian/plugins'
+    vaultPath: ''
+}; 
